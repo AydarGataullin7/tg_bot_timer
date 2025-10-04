536ac9b22208070421696ed9fa9e611051734c24
AydarGataullin7 <aydarcr7@gmail.com>
2025-10-04 22:22:06 +0500

    Add files via upload

---
+++ tg_bot.py
@@ -0,0 +1,50 @@
+import os
+import ptbot
+import pytimeparse
+from dotenv import load_dotenv
+from pytimeparse import parse
+
+
+load_dotenv()
+TG_TOKEN = os.getenv('TG_TOKEN')
+TG_CHAT_ID = os.getenv('TG_CHAT_ID')
+bot = ptbot.Bot(TG_TOKEN)
+
+
+def wait(chat_id, message):
+    seconds = parse(message)
+    message_id = bot.send_message(
+        chat_id, "Таймер установлен на: {0} секунд".format(seconds))
+    bot.create_countdown(seconds, notify_progress,
+                         chat_id=chat_id, message_id=message_id, total_seconds=seconds)
+    bot.create_timer(seconds + 0.1, answer, chat_id=chat_id)
+
+
+def notify_progress(secs_left, chat_id, message_id, total_seconds):
+    progressbar = render_progressbar(total_seconds, total_seconds - secs_left)
+    progress_message = "Осталось секунд: {0}\n{1}".format(
+        secs_left, progressbar)
+    bot.update_message(chat_id, message_id, progress_message)
+
+
+def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
+    iteration = min(total, iteration)
+    percent = "{0:.1f}"
+    percent = percent.format(100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    pbar = fill * filled_length + zfill * (length - filled_length)
+    return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
+
+
+def answer(chat_id):
+    answer = "Время вышло!"
+    bot.send_message(chat_id, answer)
+
+
+def main():
+    bot.reply_on_message(wait)
+    bot.run_bot()
+
+
+if __name__ == '__main__':
+    main()
