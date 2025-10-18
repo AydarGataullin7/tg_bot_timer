87fe32f1d375e86080d9a3f5fed94840b54c68a7
AydarGataullin7 <aydarcr7@gmail.com>
2025-10-18 22:47:00 +0500

    Add files via upload

--- tg_bot.py
+++ tg_bot.py
@@ -8,27 +8,23 @@ from decouple import config
 
 TG_TOKEN = config('TG_TOKEN')
 TG_CHAT_ID = config('TG_CHAT_ID')
+bot = ptbot.Bot(TG_TOKEN)
 
 
-def make_wait(bot):
-    def wait(chat_id, message):
-        seconds = parse(message)
-        message_id = bot.send_message(
-            chat_id, "Таймер установлен на: {0} секунд".format(seconds))
-        bot.create_countdown(seconds, make_notify_progress(bot),
-                             chat_id=chat_id, message_id=message_id, total_seconds=seconds)
-        bot.create_timer(seconds + 0.1, make_answer(bot), chat_id=chat_id)
-    return wait
+def wait(chat_id, message):
+    seconds = parse(message)
+    message_id = bot.send_message(
+        chat_id, "Таймер установлен на: {0} секунд".format(seconds))
+    bot.create_countdown(seconds, notify_progress,
+                         chat_id=chat_id, message_id=message_id, total_seconds=seconds)
+    bot.create_timer(seconds + 0.1, answer, chat_id=chat_id)
 
 
-def make_notify_progress(bot):
-    def notify_progress(secs_left, chat_id, message_id, total_seconds):
-        progressbar = render_progressbar(
-            total_seconds, total_seconds - secs_left)
-        progress_message = "Осталось секунд: {0}\n{1}".format(
-            secs_left, progressbar)
-        bot.update_message(chat_id, message_id, progress_message)
-    return notify_progress
+def notify_progress(secs_left, chat_id, message_id, total_seconds):
+    progressbar = render_progressbar(total_seconds, total_seconds - secs_left)
+    progress_message = "Осталось секунд: {0}\n{1}".format(
+        secs_left, progressbar)
+    bot.update_message(chat_id, message_id, progress_message)
 
 
 def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
@@ -40,17 +36,14 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
 
-def make_answer(bot):
-    def answer(chat_id):
-        answer_text = "Время вышло!"
-        bot.send_message(chat_id, answer_text)
-    return answer
+def answer(chat_id):
+    answer = "Время вышло!"
+    bot.send_message(chat_id, answer)
 
 
 def main():
     load_dotenv()
-    bot = ptbot.Bot(TG_TOKEN)
-    bot.reply_on_message(make_wait(bot))
+    bot.reply_on_message(wait)
     bot.run_bot()
 
 
